3f51a4c9d711f47219f15606874eb1adf3f47906
Svaym <Vaymeraa22@mail.ru>
2024-05-15 11:10:24 +0500

    second changes

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ function App() {
 
   return (
     <>
-    <h1 className="text-3xl font-bold underline text-green-400">
+    <h1 className="text-3xl font-bold underline text-green-400 uppercase">
       Hello world!
     </h1>
     </>
